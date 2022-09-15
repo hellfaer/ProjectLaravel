b2aebbbf6b6dbdb4499faf47ec579f9b67b44f23
Aleksei <hellfaer@list.ru>
2022-09-16 02:19:25 +0300

    ver 1.0

---
+++ app/Http/Controllers/BookAuthorsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookAuthorsController extends Controller
+{
+    public function index() {
+        return 'aaa74747875785785aa';
+    }
+}
