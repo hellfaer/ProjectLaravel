e1a21d1658a81ffb19dc76c1b29aa24e4d52e0d7
Aleksei <hellfaer@list.ru>
2022-09-16 04:53:38 +0300

    migration

---
+++ database/migrations/2022_09_15_234741_create_book_authors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_authors', function (Blueprint $table) {
+            $table->id();
+            $table->string('the_author');
+        });
+        
+        Schema::create('author_id', function (Blueprint $table) {
+            $table->id();
+            $table->text('name_book');
+            $table->integer('author_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_authors');
+    }
+}
